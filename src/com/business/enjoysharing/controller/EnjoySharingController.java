ad8f404b8c7a6d7ef187b5650e437c65e0a32b7c
coolguava <coolguava@sina.com>
2014-08-25 21:33:58 +0800

    测试
    测试

--- src/com/business/enjoysharing/controller/EnjoySharingController.java
+++ src/com/business/enjoysharing/controller/EnjoySharingController.java
@@ -2,5 +2,5 @@ package com.business.enjoysharing.controller;
 
 // add by chenjun
 public class EnjoySharingController {
-
+	// ...
 }
--- src/com/business/siteservice/controller/SiteServiceController.java
+++ src/com/business/siteservice/controller/SiteServiceController.java
@@ -1,5 +1,6 @@
 package com.business.siteservice.controller;
 
+// add by chenjun
 public class SiteServiceController {
 
 }
