7ea964b9accbb5494dfb24f38f8f03daa7f8f19f
coolguava <coolguava@sina.com>
2014-10-03 16:12:28 +0800

    HttpEntity学习

---
+++ src/com/example/controller/HttpEntityExampleController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/httpEntityExample")
+public class HttpEntityExampleController { // 此例没有测试通过，出现400错误
+	@RequestMapping(value = "/handle", method = RequestMethod.POST)
+	public ResponseEntity<String> handle(HttpEntity<String> requestEntity) {
+		String requestHeader = requestEntity.getHeaders().getFirst("Content-Type");
+		System.out.println("requestHeader is " + requestHeader);
+		String requestBody = requestEntity.getBody();
+		System.out.println("requestBody is " + requestBody);
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Content-Type", "application/json;charset=UTF-8");
+		return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
+	}
+}
