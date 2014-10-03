5d91cc6ca67c6477f90799f30732eabddd19d973
coolguava <coolguava@sina.com>
2014-10-03 16:32:07 +0800

    @RequestHeader学习

--- src/com/example/controller/HttpEntityExampleController.java
+++ src/com/example/controller/HttpEntityExampleController.java
@@ -12,11 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/httpEntityExample")
 public class HttpEntityExampleController { // 此例没有测试通过，出现400错误
 	@RequestMapping(value = "/handle", method = RequestMethod.POST)
-	public ResponseEntity<String> handle(HttpEntity<String> requestEntity) {
+	public ResponseEntity<String> handle(HttpEntity<byte[]> requestEntity) {
 		String requestHeader = requestEntity.getHeaders().getFirst("Content-Type");
 		System.out.println("requestHeader is " + requestHeader);
-		String requestBody = requestEntity.getBody();
-		System.out.println("requestBody is " + requestBody);
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.set("Content-Type", "application/json;charset=UTF-8");
 		return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
---
+++ src/com/example/controller/RequestHeaderExampleController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.javabeans.Human;
+
+@Controller
+@RequestMapping(value = "/requestHeaderExample")
+public class RequestHeaderExampleController {
+	@RequestMapping(value = "/handle", method = RequestMethod.POST)
+	public @ResponseBody Human handle(@RequestBody Human human,
+			@RequestHeader("Accept") String accept,
+			@RequestHeader("Accept-Language") String acceptLang,
+			@RequestHeader("Accept-Encoding") String acceptEnc,
+			@RequestHeader("User-Agent") String userAgent) {
+		System.out.println("accept >>>> " + accept);
+		System.out.println("acceptLang >>>> " + acceptLang);
+		System.out.println("acceptEnc >>>> " + acceptEnc);
+		System.out.println("userAgent >>>> " + userAgent);
+		human.setHobby("篮球");
+		return human;
+	}
+}
