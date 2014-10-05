5fa95752dfc81739fab210aa9326013646488c54
coolguava <coolguava@sina.com>
2014-10-05 23:54:14 +0800

    Beetl~~使用模版文件来作为方法

---
+++ src/com/example/beetl/PageTest.java
@@ -0,0 +1,20 @@
+package com.example.beetl;
+
+import java.io.IOException;
+
+import org.beetl.core.Configuration;
+import org.beetl.core.GroupTemplate;
+import org.beetl.core.Template;
+import org.beetl.core.resource.ClasspathResourceLoader;
+
+public class PageTest {
+	public static void main(String[] args) throws IOException {
+		ClasspathResourceLoader loader = new ClasspathResourceLoader();
+		Configuration configuration = Configuration.defaultConfiguration();
+		GroupTemplate gt = new GroupTemplate(loader, configuration);
+		Template template = gt.getTemplate("/com/example/beetl/Page.btl");
+		template.binding("current", "1");
+		template.binding("total", "10");
+		System.out.println(template.render());
+	}
+}
