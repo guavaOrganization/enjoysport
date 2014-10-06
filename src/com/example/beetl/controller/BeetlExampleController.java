32c2427ac90302a5c7248ff0b78cddb3a36c3168
coolguava <coolguava@sina.com>
2014-10-06 11:10:49 +0800

    Spring beetl 融合改造

--- src/com/example/beetl/controller/BeetlExampleController.java
+++ src/com/example/beetl/controller/BeetlExampleController.java
@@ -10,6 +10,7 @@ public class BeetlExampleController {
 	@RequestMapping(value = "/helloWorld")
 	public String helloWorld(Model model) {
 		model.addAttribute("name", "陈俊");
+		model.addAttribute("title", "Beetl Spring 融合");
 		return "beetl/helloWorld";
 	}
 }
